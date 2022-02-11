a8e45b261dea1adb17bcbfa79b03ab74003e897b
mariokordic <mario.kordic4@gmail.com>
2022-02-11 10:37:18 +0100

    dodavanje svojstva klasi

---
+++ Frizerski_zavrsni/src/main/java/edunova/model/Djelatnik.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edunova.model;
+
+/**
+ *
+ * @author mkordic
+ */
+public class Djelatnik {
+        private int sifra;
+        private String ime;
+	private String prezime;
+	private String adresa;
+	private String grad;
+	private String drzava;
+	private int postanskiBroj; 
+
+    public int getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(int sifra) {
+        this.sifra = sifra;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public void setAdresa(String adresa) {
+        this.adresa = adresa;
+    }
+
+    public String getGrad() {
+        return grad;
+    }
+
+    public void setGrad(String grad) {
+        this.grad = grad;
+    }
+
+    public String getDrzava() {
+        return drzava;
+    }
+
+    public void setDrzava(String drzava) {
+        this.drzava = drzava;
+    }
+
+    public int getPostanskiBroj() {
+        return postanskiBroj;
+    }
+
+    public void setPostanskiBroj(int postanskiBroj) {
+        this.postanskiBroj = postanskiBroj;
+    }
+        
+        
+}
---
+++ Frizerski_zavrsni/src/main/java/edunova/model/Korisnik.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edunova.model;
+
+/**
+ *
+ * @author mkordic
+ */
+public class Korisnik {
+        private int sifra;
+        private String ime;
+	private String prezime;
+	private String brojMobitela;
+	private String adresa;
+	private String grad;
+	private String drzava;
+	private int postanskiBroj;
+	private char spol; 
+
+        public int getSifra() {
+            return sifra;
+        }
+
+        public void setSifra(int sifra) {
+            this.sifra = sifra;
+        }
+
+        public String getIme() {
+            return ime;
+        }
+
+        public void setIme(String ime) {
+            this.ime = ime;
+        }
+
+        public String getPrezime() {
+            return prezime;
+        }
+
+        public void setPrezime(String prezime) {
+            this.prezime = prezime;
+        }
+
+        public String getBrojMobitela() {
+            return brojMobitela;
+        }
+
+        public void setBrojMobitela(String brojMobitela) {
+            this.brojMobitela = brojMobitela;
+        }
+
+        public String getAdresa() {
+            return adresa;
+        }
+
+        public void setAdresa(String adresa) {
+            this.adresa = adresa;
+        }
+
+        public String getGrad() {
+            return grad;
+        }
+
+        public void setGrad(String grad) {
+            this.grad = grad;
+        }
+
+        public String getDrzava() {
+            return drzava;
+        }
+
+        public void setDrzava(String drzava) {
+            this.drzava = drzava;
+        }
+
+        public int getPostanskiBroj() {
+            return postanskiBroj;
+        }
+
+        public void setPostanskiBroj(int postanskiBroj) {
+            this.postanskiBroj = postanskiBroj;
+        }
+
+        public char getSpol() {
+            return spol;
+        }
+
+        public void setSpol(char spol) {
+            this.spol = spol;
+        }
+        
+        
+}
